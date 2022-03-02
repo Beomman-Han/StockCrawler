3bba882d9c6ec37102cee98d0e0db246873903e2
Beomman-Han <qjaaks6378@gmail.com>
2022-03-02 19:01:55 +0900

    Trim '_scrap_naver_fin_page' method in KOSPICrawler

--- KOSPICrawler.py
+++ KOSPICrawler.py
@@ -28,25 +28,25 @@ class CorpInfo(object):
         
         return
 
-class KOSPICrawler(Crawler):
+class KOSPICrawler:
     """KOSPI 종목 관련 재무 정보를 크롤링하기 위한 클래스
     """
     
     KOSPI_CODE = 'Y'
 
-    def __init__(self, csv: str) -> None:
-        """Initialize KOSPICrawler class 
+    # def __init__(self, csv: str) -> None:
+    #     """Initialize KOSPICrawler class 
         
-        Parameters
-        ----------
-        csv : str
-            Path of .csv file containing corp info
-        """
+    #     Parameters
+    #     ----------
+    #     csv : str
+    #         Path of .csv file containing corp info
+    #     """
         
-        # self.stock_data = './data/test_samsung.2021-12-12.csv'
-        self.csv = csv
+    #     # self.stock_data = './data/test_samsung.2021-12-12.csv'
+    #     self.csv = csv
         
-        return
+    #     return
 
     def _load_corp_info(
         self,
@@ -74,13 +74,132 @@ class KOSPICrawler(Crawler):
 
         return corps
 
+    def _scrap_naver_fin_page(self,
+        corp_code : str,
+        corp_name : str,
+        stock_code: str
+        ) -> CorpFinance:
+        
+        """기업 관련 정보로부터 네이버 금융에서 재무 정보를 수집한다
+        
+        ** 수집할 정보
+            시가총액, 상장주식수, PER, PBR, 동일업종 PER,
+            (3년) 매출액, 영업이익, 당기순이익,
+            영업이익률, 순이익률, ROE(**),
+            부채비율, 당좌비율, PER, PBR
+
+        FIXME 추가해야 할 정보
+            (최근 분기) 매출액, 영업이익, 당기순이익,
+            영억이익률, 순이익률, ROE(**),
+            부채비율, 당좌비율, PER, PBR
+
+        FIXME + 위 정보를 포함할 데이터 저장용 클래스를 생성 (FinanceInfo())
+
+        Parameters
+        ----------
+        corp_code: str
+            기업 고유 코드번호
+        corp_name: str
+            기업 이름
+        stock_code: str
+            주식 고유 코드번호
+
+        Returns
+        -------
+        CorpFinance
+            각 정보(재무정보 포함)가 포함된 CorpFinance 클래스
+        """
+
+        print(f'[Running] Scrap naver page: {corp_name}, {corp_code}, {stock_code}')
+
+        basic_url = 'https://finance.naver.com/item/main.naver'
+        
+        ## 외부 프로그램이 아닌 정상적인 형태로 접근하는 것처럼 보이게 함(?)
+        header = {'User-Agent': 'Mozilla/5.0', 'referer': 'http://naver.com'}
+        ## 삼성전자 - https://finance.naver.com/item/main.naver?code=005930
+        param  = {'code': stock_code}
+        
+        response = self._response_url(basic_url, {'header': header, 'param': param})
+        print(f'[Running] Response status code: {response.status_code}')
+        
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.text, 'html.parser')
+        else:
+            return CorpFinance('', '', '')
+
+        corp_fin = CorpFinance(corp_name, corp_code, stock_code)
+
+        ## FIXME 정상 금융 정보 페이지인지 확인
+
+        ## 시가총액 및 상장주식수
+        market_capital = self._soup_get_text(soup, '#_market_sum')
+
+        css_selector  = '#tab_con1 > div.first > table > tr:nth-of-type(3) > td > em'
+        stocks_listed = self._soup_get_text(soup, css_selector)
+        if stocks_listed is not None:
+            stocks_listed = int(stocks_listed.replace(',',''))
+
+        corp_fin.set_market_cap(market_capital)
+        corp_fin.set_stocks_listed(stocks_listed)
+
+        if market_capital is not None and stocks_listed is not None:
+            css_corp_analysis = f'div.section.cop_analysis'
+
+            ## 과거 3개년 년도
+            years = []
+            for i in range(1, 4):
+                css_selector = f'{css_corp_analysis} > div.sub_section > table > thead > tr:nth-of-type(2) > th:nth-of-type({i})'
+                year = self._soup_get_text(soup, css_selector)
+                years.append(year)
+
+            ## 재무정보 단위 주의
+            financial_item = ['revenue', 'operating_income', 'net_profit',\
+                              'operating_income_rate', 'net_profit_rate',\
+                              'roe', 'liability_rate', 'quick_rate', 'per', 'pbr']
+            
+            total_fin_info = dict()
+            for f_idx, item in enumerate(financial_item):
+                if item == 'per':
+                    item_css_selector = f'{css_corp_analysis} > div.sub_section > table > tbody > tr:nth-of-type({11})'
+                elif item == 'pbr':
+                    item_css_selector = f'{css_corp_analysis} > div.sub_section > table > tbody > tr:nth-of-type({13})'
+                else:
+                    item_css_selector = f'{css_corp_analysis} > div.sub_section > table > tbody > tr:nth-of-type({f_idx+1})'
+                each_fin_info = dict()
+                for y_idx, year in enumerate(years):
+                    css_selector = f'{item_css_selector} > td:nth-of-type({y_idx+1})'
+                    #tag = soup.select_one(css_selector)
+                    tag = self._soup_get_text(soup, css_selector)
+                    if tag is not None:
+                        tag = tag.replace(',','')
+                        tag = self._numeric(tag)
+                    each_fin_info[year] = tag
+                total_fin_info[item] = each_fin_info
+
+            ## store financial info
+            corp_fin.set_revenue(total_fin_info['revenue'])
+            corp_fin.set_operating_income(total_fin_info['operating_income'])
+            corp_fin.set_operating_income_rate(total_fin_info['operating_income_rate'])
+            corp_fin.set_net_profit(total_fin_info['net_profit'])
+            corp_fin.set_net_profit_rate(total_fin_info['net_profit_rate'])
+            corp_fin.set_roe(total_fin_info['roe'])
+            corp_fin.set_liability_rate(total_fin_info['liability_rate'])
+            corp_fin.set_quick_rate(total_fin_info['quick_rate'])
+            corp_fin.set_per(total_fin_info['per'])
+            corp_fin.set_pbr(total_fin_info['pbr'])
+
+        return corp_fin
+
     def _set_api_key(self) -> None:
         """DART Open 서비스 이용을 위한 api_key 입력"""
         dart.set_api_key(api_key='1bf8a5d812f3e051f08b1cb262b4b4bd6c9b1ced')
         return
 
-    def _response_url(self, url, params_: dict) -> requests.models.Response:
-        """url request를 보내 response를 받음.
+    def _response_url(self,
+        url : str,
+        params_ : dict
+        ) -> requests.models.Response:
+        """URL request를 보내 response를 받음.
 
         Parameters
         ----------
@@ -171,198 +290,6 @@ class KOSPICrawler(Crawler):
 
         return val
 
-    def _scrap_naver_fin_page(self, corp_code: str, corp_name: str, stock_code: str) -> CorpFinance:
-        """주식 코드를 받아 네이버 금융 사이트에서 관련 정보를 수집한다.
-        수집할 정보는 다음과 같다.
-        > 시가총액, 상장주식수, PER, PBR, 동일업종 PER,
-          (3년) 매출액, 영업이익, 당기순이익,
-          영업이익률, 순이익률, ROE(**),
-          부채비율, 당좌비율, PER, PBR
-
-        FIXME 추가해야 할 정보
-        > (최근 분기) 매출액, 영업이익, 당기순이익,
-        영억이익률, 순이익률, ROE(**),
-        부채비율, 당좌비율, PER, PBR
-
-        FIXME + 위 정보를 포함할 데이터 저장용 클래스를 생성 (FinanceInfo())
-
-        Parameters
-        ----------
-        corp_code: str
-            기업 고유 코드번호
-        corp_name: str
-            기업 이름
-        stock_code: str
-            주식 고유 코드번호
-
-        Returns
-        -------
-        CorpFinance
-            각 정보(재무정보 포함)가 포함된 CorpFinance 클래스
-        """
-
-        print(f'> Scrap naver page: {corp_name}, {corp_code}, {stock_code}')
-        #corp_info = OrderedDict()
-
-        basic_url = 'https://finance.naver.com/item/main.naver'
-        ## 외부 프로그램이 아닌 정상적인 형태로 접근하는 것처럼 보이게 함(?)
-        header = {'User-Agent': 'Mozilla/5.0', 'referer': 'http://naver.com'}
-        param  = {'code': stock_code}
-        response = self._response_url(basic_url, {'header': header, 'param': param})
-        print(f'response status code: {response.status_code}')
-        if response.status_code == 200:
-            soup = BeautifulSoup(response.text, 'html.parser')
-        else:
-            return CorpFinance('', '', '')
-
-        corp_fin = CorpFinance(corp_name, corp_code, stock_code)
-
-        ## 정상 금융 정보 페이지인지 확인
-        """
-        css_selector = '#middle > div.h_company > div.wrap_company > h2 > a'
-        try:
-            chk = soup.select_one(css_selector).get_text().strip()
-        except AttributeError:
-        """
-
-        ## 시가총액 및 상장주식수
-        """
-        try:
-            market_capital = soup.select_one('#_market_sum').get_text()
-            market_capital = int(market_capital.strip().replace('\n', '').replace('\t', '').replace(',', '').replace('조', ''))
-        except AttributeError:
-            market_capital = None
-        if market_capital is not None:
-            if '경' in market_capital:
-                market_capital = 100000000  ## 경 이상...?
-            elif '조' in market_capital:
-                market_capital = market_capital.replace(',','').split('조')
-                if len(market_capital) == 2:
-                    market_capital = 1000*int(market_capital[0]) + int(market_capital[1])
-        #print(f'Market capital: {market_capital}')
-        
-        try:
-            stocks_listed = soup.select_one('#tab_con1 > div.first > table > tr:nth-of-type(3) > td > em').get_text()
-            stocks_listed = int(stocks_listed.strip().replace(',','').replace('\n','').replace('\t',''))
-        except AttributeError:
-            stocks_listed = None        
-        """
-
-        market_capital = self._soup_get_text(soup, '#_market_sum')
-
-        css_selector  = '#tab_con1 > div.first > table > tr:nth-of-type(3) > td > em'
-        stocks_listed = self._soup_get_text(soup, css_selector)
-        if stocks_listed is not None:
-            stocks_listed = int(stocks_listed.replace(',',''))
-
-        corp_fin.set_market_cap(market_capital)
-        corp_fin.set_stocks_listed(stocks_listed)
-
-        if market_capital is not None and stocks_listed is not None:
-            css_corp_analysis = f'div.section.cop_analysis'
-
-            ## 과거 3개년 년도
-            """
-            > 과거 3개년 연도 정보
-            '#content > div.section.cop_analysis > div.sub_section > table > thead > tr:nth-child(2) > th:nth-child(1)'
-            '#content > div.section.cop_analysis > div.sub_section > table > thead > tr:nth-child(2) > th:nth-child(2)'
-            '#content > div.section.cop_analysis > div.sub_section > table > thead > tr:nth-child(2) > th:nth-child(3)'
-    
-            > 최근 5개 분기 분기 정보
-            '#content > div.section.cop_analysis > div.sub_section > table > thead > tr:nth-child(2) > th:nth-child(5)'
-            '#content > div.section.cop_analysis > div.sub_section > table > thead > tr:nth-child(2) > th:nth-child(6)'
-            ...
-            
-            '#content > div.section.cop_analysis > div.sub_section > table > thead > tr:nth-child(2) > th:nth-child(1)'    
-            """
-            years = []
-            for i in range(1, 4):
-                css_selector = f'{css_corp_analysis} > div.sub_section > table > thead > tr:nth-of-type(2) > th:nth-of-type({i})'
-                year = self._soup_get_text(soup, css_selector)
-                years.append(year)
-            #print(years); sys.exit()
-
-            """
-            > 재무정보 단위 (억원??)
-            매출액:    '#content > div.section.cop_analysis > div.sub_section > table > tbody > tr:nth-child(1) > th'
-            영억이익:   '#content > div.section.cop_analysis > div.sub_section > table > tbody > tr:nth-child(2) > th'
-            당기순이익: '#content > div.section.cop_analysis > div.sub_section > table > tbody > tr:nth-child(3) > th'
-            
-            financial_info = ['매출액', '영업이익', '당기순이익',\
-                              '영업이익률', '순이익률', 'ROE',\
-                              '부채비율', '당좌비율', 'PER', 'PBR']
-            #first_fin = ['매출액', '영업이익', '당기순이익']
-            for i in range(1, 9):
-                css_selector = f'div.sub_section > table > tbody > tr:nth-child({i}) > th'
-                unit = corp_analysis.select_one(css_selector)
-                unit = unit.get_text().strip()
-            """
-            """
-            > 과거 3년 주요 재무정보
-            매출액: '#content > div.section.cop_analysis > div.sub_section > table > tbody > tr:nth-child(1) > td:nth-child(2,3,4)'
-            영업익: '#content > div.section.cop_analysis > div.sub_section > table > tbody > tr:nth-child(2) > td:nth-child(2,3,4)'
-            당기익: '#content > div.section.cop_analysis > div.sub_section > table > tbody > tr:nth-child(3) > td:nth-child(2,3,4)'
-            """
-            financial_item = ['revenue', 'operating_income', 'net_profit',\
-                              'operating_income_rate', 'net_profit_rate',\
-                              'roe', 'liability_rate', 'quick_rate', 'per', 'pbr']
-
-            """
-            '#content > div.section.cop_analysis > div.sub_section > table > tbody > tr:nth-child(1) > td:nth-child(2)'
-            '#content > div.section.cop_analysis > div.sub_section > table > tbody > tr:nth-child(2) > td:nth-child(2)'
-            '#content > div.section.cop_analysis > div.sub_section > table > tbody > tr:nth-child(11) > td:nth-child(2)'
-            '#content > div.section.cop_analysis > div.sub_section > table > tbody > tr:nth-child(13) > td:nth-child(2)'
-            #content > div.section.cop_analysis > div.sub_section > table > tbody > tr:nth-child(11) > td:nth-child(2)
-            """
-            total_fin_info = dict()
-            for f_idx, item in enumerate(financial_item):
-                if item == 'per':
-                    item_css_selector = f'{css_corp_analysis} > div.sub_section > table > tbody > tr:nth-of-type({11})'
-                elif item == 'pbr':
-                    item_css_selector = f'{css_corp_analysis} > div.sub_section > table > tbody > tr:nth-of-type({13})'
-                else:
-                    item_css_selector = f'{css_corp_analysis} > div.sub_section > table > tbody > tr:nth-of-type({f_idx+1})'
-                each_fin_info = dict()
-                for y_idx, year in enumerate(years):
-                    css_selector = f'{item_css_selector} > td:nth-of-type({y_idx+1})'
-                    #tag = soup.select_one(css_selector)
-                    tag = self._soup_get_text(soup, css_selector)
-                    if tag is not None:
-                        tag = tag.replace(',','')
-                        tag = self._numeric(tag)
-                    """
-                    try:
-                        tag_text = tag.get_text().strip().replace(',', '')  ## '2,437,714' -> '2437714'
-                    except AttributeError:
-                        tag_text = None
-                        tag_val  = None
-                        each_fin_info[year] = tag_val
-                        continue
-
-                    if tag_text == '':
-                        each_fin_info[year] = None
-                        continue
-
-                    try:
-                        tag_val = int(tag_text)
-                    except ValueError:
-                        tag_val = float(tag_text)
-                    """
-                    each_fin_info[year] = tag
-                total_fin_info[item] = each_fin_info
-
-            corp_fin.set_revenue(total_fin_info['revenue'])
-            corp_fin.set_operating_income(total_fin_info['operating_income'])
-            corp_fin.set_operating_income_rate(total_fin_info['operating_income_rate'])
-            corp_fin.set_net_profit(total_fin_info['net_profit'])
-            corp_fin.set_net_profit_rate(total_fin_info['net_profit_rate'])
-            corp_fin.set_roe(total_fin_info['roe'])
-            corp_fin.set_liability_rate(total_fin_info['liability_rate'])
-            corp_fin.set_quick_rate(total_fin_info['quick_rate'])
-            corp_fin.set_per(total_fin_info['per'])
-            corp_fin.set_pbr(total_fin_info['pbr'])
-
-        return corp_fin
 
     @property
     def url(self) -> str:
@@ -434,7 +361,10 @@ if __name__ == '__main__':
     #url = 'https://finance.naver.com/item/main.naver?code=005930'
     #KOSPICrawler(url)
     #load_corp_code()
-    # crawler = KOSPICrawler()
+    crawler = KOSPICrawler()
+    corp = crawler._scrap_naver_fin_page('00159616', '두산중공업', '034020')
+    print(corp)
+    
     #crawler.run_naver()
     #corp = crawler._scrap_naver_fin_page('00177816','대주전자재료','078600')
     #corp = crawler._scrap_naver_fin_page('00126308','삼성엔지니어링','028050')
